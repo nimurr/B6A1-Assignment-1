4860e0ca8fa6eca964f1fcc7b5e396dcbfe2a9b1
nimurr <nimurnerob404@gmail.com>
2025-11-16 23:07:40 +0600

    Solution 1 ,2,4 testing completed done

--- solution.ts
+++ solution.ts
@@ -40,14 +40,8 @@ const getLength = (value: string | string[] | number[] | any[]) => {
 // console.log(person1.getDetails());
 
 //? Solution 4 :-
+
 const filterByRating = (data: { title: string, rating: number }[]) => {
     return data.filter((item) => item.rating >= 4);
 }
 
-const books = [
-    { title: 'Book A', rating: 4.5 },
-    { title: 'Book B', rating: 3.2 },
-    { title: 'Book C', rating: 5.0 },
-];
-
-console.log(filterByRating(books));
