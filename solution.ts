d2589788c56431f6ad3411b678ae996aab1a0120
nimurr <nimurnerob404@gmail.com>
2025-11-16 22:58:11 +0600

    Problem 2 solution completed Done

--- solution.ts
+++ solution.ts
@@ -11,9 +11,42 @@ const formatValue = (value: string | number | boolean) => {
     }
 };
 
-console.log(formatValue("hello"));
-console.log(formatValue(10));
-console.log(formatValue(false));
+
 
 //? Solution 2 :-
 
+const getLength = (value: string | string[] | number[] | any[]) => {
+
+    if (Array.isArray(value)) {
+        return value.length;
+    }
+    else {
+        return value.length;
+    }
+
+}
+
+//? Solution 3 :-
+
+// class Person {
+//     constructor(public name: string, public age: number) { }
+//     getDetails(){
+//         return `Name: ${this.name}, Age: ${this.age}`
+//     }
+// }
+
+// const person1 = new Person("Nerob" , 24);
+// console.log(person1.getDetails());
+
+
+class Person {
+  constructor(public name: string, public age: number) {}
+
+  getDetails() {
+    return `Name: ${this.name}, Age: ${this.age}`;
+  }
+}
+
+// Example usage:
+const person1 = new Person("Nimur Rahman", 22);
+console.log(person1.getDetails());
